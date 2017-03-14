4ef0cc5b4d9c00a477257a9869f0697bdfd749f4
Will Merkes <merkesw@outlook.com>
2017-03-14 13:34:16 -0500

    User following

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'carrierwave'
@@ -10,8 +11,6 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true  
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,5 +73,34 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    rails    = users(:rails)
+    archer   = users(:archer)
+    assert_not rails.following?(archer)
+    rails.follow(archer)
+    assert rails.following?(archer)
+    assert archer.followers.include?(rails)
+    rails.unfollow(archer)
+    assert_not rails.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    rails   = users(:rails)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    lana.microposts.each do |post_following|
+      assert rails.feed.include?(post_following)
+    end
+    
+    rails.microposts.each do |post_self|
+      assert rails.feed.include?(post_self)
+    end
+
+    archer.microposts.each do |post_unfollowed|
+      assert_not rails.feed.include?(post_unfollowed)
+    end
+  end
+
 end
 
